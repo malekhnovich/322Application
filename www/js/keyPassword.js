5f55c69e45275350af77c6fc679c2f5879873053
malekhnovich <maxrenet@Maxs-MacBook-Pro-2.local>
2016-02-29 12:19:43 -0500

    322Application

---
+++ www/js/keyPassword.js
@@ -0,0 +1,8 @@
+/**
+ * Created by maxrenet on 2/27/16.
+ */
+// Your AWS Access Key ID, as taken from the AWS Your Account page
+var aws_acces_key_id= "AKIAJYFONCHRZDHPYF2A";
+
+// Your AWS Secret Key corresponding to the above ID, as taken from the AWS Your Account page
+var aws_secret_key = "mvN9htSLP6WDQLceXq1NFFNpTepXy/36kclu2P7B";
